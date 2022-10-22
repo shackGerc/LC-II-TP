1df7f60c6c61e021f70188c8101681a654474285
shackgerc <sagerprogramer@gmail.com>
2022-10-22 16:30:57 -0300

    'TP'

---
+++ wfrmFrontTest/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace wfrmFrontTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.sideBar = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.PbExpand = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SideBarTimer = new System.Windows.Forms.Timer(this.components);
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.PbNotExpand = new System.Windows.Forms.PictureBox();
+            this.sideBar.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PbExpand)).BeginInit();
+            this.panel4.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PbNotExpand)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sideBar
+            // 
+            this.sideBar.BackColor = System.Drawing.Color.Aquamarine;
+            this.sideBar.Controls.Add(this.panel1);
+            this.sideBar.Controls.Add(this.panel4);
+            this.sideBar.Controls.Add(this.panel2);
+            this.sideBar.Controls.Add(this.panel3);
+            this.sideBar.Location = new System.Drawing.Point(0, 0);
+            this.sideBar.MaximumSize = new System.Drawing.Size(214, 574);
+            this.sideBar.MinimumSize = new System.Drawing.Size(62, 574);
+            this.sideBar.Name = "sideBar";
+            this.sideBar.Size = new System.Drawing.Size(214, 574);
+            this.sideBar.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.PbNotExpand);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.PbExpand);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(211, 59);
+            this.panel1.TabIndex = 0;
+            // 
+            // PbExpand
+            // 
+            this.PbExpand.Image = ((System.Drawing.Image)(resources.GetObject("PbExpand.Image")));
+            this.PbExpand.Location = new System.Drawing.Point(9, 14);
+            this.PbExpand.Name = "PbExpand";
+            this.PbExpand.Padding = new System.Windows.Forms.Padding(0, 0, 0, 50);
+            this.PbExpand.Size = new System.Drawing.Size(32, 35);
+            this.PbExpand.TabIndex = 0;
+            this.PbExpand.TabStop = false;
+            this.PbExpand.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(74, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Menu";
+            // 
+            // SideBarTimer
+            // 
+            this.SideBarTimer.Interval = 10;
+            this.SideBarTimer.Tick += new System.EventHandler(this.SideBarTimer_Tick);
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.button3);
+            this.panel4.Location = new System.Drawing.Point(3, 68);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(197, 60);
+            this.panel4.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(-25, -10);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(250, 78);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Soporte";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(3, 134);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(197, 60);
+            this.panel2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(-25, -10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(250, 78);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Info";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Location = new System.Drawing.Point(3, 200);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(197, 60);
+            this.panel3.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(-25, -10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(250, 78);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Salir";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // PbNotExpand
+            // 
+            this.PbNotExpand.Image = ((System.Drawing.Image)(resources.GetObject("PbNotExpand.Image")));
+            this.PbNotExpand.Location = new System.Drawing.Point(9, 14);
+            this.PbNotExpand.Name = "PbNotExpand";
+            this.PbNotExpand.Padding = new System.Windows.Forms.Padding(0, 0, 0, 50);
+            this.PbNotExpand.Size = new System.Drawing.Size(32, 35);
+            this.PbNotExpand.TabIndex = 2;
+            this.PbNotExpand.TabStop = false;
+            this.PbNotExpand.Visible = false;
+            this.PbNotExpand.Click += new System.EventHandler(this.PbNotExpand_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1005, 574);
+            this.Controls.Add(this.sideBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximumSize = new System.Drawing.Size(1023, 621);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.sideBar.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PbExpand)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.PbNotExpand)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel sideBar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox PbExpand;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer SideBarTimer;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox PbNotExpand;
+    }
+}
+
